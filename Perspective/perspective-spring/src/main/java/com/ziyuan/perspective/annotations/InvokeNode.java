0f9d089005c3cf3b7faa7186c780d23476f3b455
ziyuan <ziyuan@2dfire.com>
2017-02-28 16:18:57 +0800

    dubbo扩展 filter,spring扩展aspect

---
+++ Perspective/perspective-spring/src/main/java/com/ziyuan/perspective/annotations/InvokeNode.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.ziyuan.perspective.annotations;
+
+/**
+ * InvokeNode
+ *
+ * @author ziyuan
+ * @since 2017-02-28
+ */
+public @interface InvokeNode {
+}
---
+++ Perspective/perspective-spring/src/main/java/com/ziyuan/perspective/aspect/InvokeAspect.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.ziyuan.perspective.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * InvokeAspect
+ *
+ * @author ziyuan
+ * @since 2017-02-28
+ */
+@Aspect
+public class InvokeAspect {
+}
