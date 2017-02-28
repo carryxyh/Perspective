e47391d1d3ac205d44d2c8772d27c1957ef43717
ziyuan <ziyuan@2dfire.com>
2017-02-28 16:12:14 +0800

    dubbo扩展 filter

---
+++ Perspective/perspective-dubbo/src/main/java/com/ziyuan/perspective/filter/PerspectiveFilter.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.ziyuan.perspective.filter;
+
+import com.alibaba.dubbo.rpc.*;
+
+/**
+ * PerspectiveFilter perspective的filter
+ *
+ * @author ziyuan
+ * @since 2017-02-28
+ */
+public class PerspectiveFilter implements Filter {
+
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        return null;
+    }
+}
---
+++ Perspective/perspective-dubbo/src/main/java/com/ziyuan/perspective/filter/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by xiuyuhang on 17/2/20.
+ * <p>
+ * 存放过滤器
+ */
+package com.ziyuan.perspective.filter;
