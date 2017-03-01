d00ab4fd6abdc9bb2fb6ffb8476be5f837b810da
ziyuan <ziyuan@2dfire.com>
2017-03-01 14:40:07 +0800

    这个版本的过滤器有点问题：1.代码冗余太多，很多异常处理应该放出去  2.模型有点小问题，需要调整

--- Perspective/perspective-dubbo/src/main/java/com/ziyuan/perspective/filter/PerspectiveFilter.java
+++ Perspective/perspective-dubbo/src/main/java/com/ziyuan/perspective/filter/PerspectiveFilter.java
@@ -6,8 +6,12 @@ package com.ziyuan.perspective.filter;
 import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.common.utils.StringUtils;
 import com.alibaba.dubbo.rpc.*;
+import com.ziyuan.perspective.Exception.SymbolIdEmptyException;
 import com.ziyuan.perspective.LocalTraceManager;
+import com.ziyuan.perspective.invokes.Invoke;
 import com.ziyuan.perspective.invokes.Trace;
+import com.ziyuan.perspective.storages.Storage;
+import com.ziyuan.perspective.util.StorageUtil;
 
 /**
  * PerspectiveFilter perspective的filter
@@ -28,20 +32,53 @@ public class PerspectiveFilter implements Filter {
      */
     private static final String P_OWN_BRANCH_ID = "own_branch_id";
 
-    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+    private Storage storage = StorageUtil.getStorage();
 
-        String traceId = getCentreTraceId();
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        boolean isFirstTier = isInitTier();
+        String traceId = getCentreTraceId(isFirstTier);
         if (StringUtils.isBlank(traceId)) {
             //TODO 这里需要记录日志为什么拿不到TraceId
             return invoker.invoke(invocation);
         }
 
-        String branchId = getOwnBranchId();
+        Invoke ownerInvoke;
+        if (isFirstTier) {
+            //第一层
+            try {
+                ownerInvoke = LocalTraceManager.getManager().get();
+            } catch (Exception e) {
 
+            }
+        } else {
+            String branchId = invocation.getAttachment(P_OWN_BRANCH_ID);
+            try {
+                ownerInvoke = storage.findBranch(traceId, branchId);
+            } catch (SymbolIdEmptyException e) {
+                //TODO log
+                return invoker.invoke(invocation);
+            }
+        }
+
+        RpcContext context = RpcContext.getContext();
         //false说明世consumer side
-        boolean isProvider = RpcContext.getContext().isProviderSide();
+        boolean isProvider = context.isProviderSide();
+        if (isProvider) {
+            //provider需要把invocation中的两个Id存下来 以便后面使用
+            context.setAttachment(P_CENTRE_TRACE_ID, traceId);
+            context.setAttachment(P_OWN_BRANCH_ID, invocation.getAttachment(P_OWN_BRANCH_ID));
+        } else {
+            //这里需要开启一个branch 把这个branch存入owner branch中
 
-        Result result = invoker.invoke(invocation);
+        }
+
+        Result result;
+        try {
+            result = invoker.invoke(invocation);
+        } catch (RpcException e) {
+            //添加ender
+            throw e;
+        }
         return result;
     }
 
@@ -50,7 +87,30 @@ public class PerspectiveFilter implements Filter {
      *
      * @return traceId
      */
-    private String getCentreTraceId() {
+    private String getCentreTraceId(boolean isFirstTier) {
+        if (isFirstTier) {
+            LocalTraceManager localTraceManager = LocalTraceManager.getManager();
+            Trace trace = null;
+            try {
+                trace = localTraceManager.get();
+            } catch (Exception e) {
+                // ignore
+            }
+            return trace.getTraceId();
+        } else {
+            //说明这里已经不是API层了,从中央存储里取Id
+            RpcContext context = RpcContext.getContext();
+            String traceId = context.getAttachment(P_CENTRE_TRACE_ID);
+            return traceId;
+        }
+    }
+
+    /**
+     * 如果是第一层，可以从中拿到trace 如果不是 threadlocal失效拿不到
+     *
+     * @return 是否是第一层（一般是API层）
+     */
+    private boolean isInitTier() {
         LocalTraceManager localTraceManager = LocalTraceManager.getManager();
         Trace trace = null;
         try {
@@ -58,23 +118,6 @@ public class PerspectiveFilter implements Filter {
         } catch (Exception e) {
             //TODO 记录日志
         }
-        if (trace == null) {
-            //说明这里已经不是API层了,从中央存储里取Id
-            RpcContext context = RpcContext.getContext();
-            String traceId = context.getAttachment(P_CENTRE_TRACE_ID);
-            return traceId;
-        }
-        return trace.getTraceId();
-    }
-
-    /**
-     * 获得branchId
-     *
-     * @return branchId
-     */
-    private String getOwnBranchId() {
-        RpcContext context = RpcContext.getContext();
-        String branchId = context.getAttachment(P_OWN_BRANCH_ID);
-        return branchId;
+        return trace != null;
     }
 }
